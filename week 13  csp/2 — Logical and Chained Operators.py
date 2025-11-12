537b8dffc545c288dd85b026a3ec3f744a7250e3
markoooooooooooo <mimunoz4@cps.edu>
2025-11-12 20:25:37 +0000

    week13

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -18,6 +18,22 @@ print(x < 5 or x == 10)   # True
 print(not(x == 10))       # False
 print(1 < x < 20)         # True
 
+# score calculator
+score = int(input("Enter your score (0-100): "))
+if 90 <= score < 100:
+    print("Grade: A")
+
+elif 80 <= score < 90:
+    print("Grade: B")
+
+elif 70 <= score < 80:
+    print("Grade: C")
+
+elif 60 <= score < 70:
+    print("Grade: D")
+
+else: 
+    print("Grade: F")
 
 # Practice Problems:
 
