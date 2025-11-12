f464b7558733b1cb293387edf912f6b34cc9719a
markoooooooooooo <mimunoz4@cps.edu>
2025-11-12 20:30:38 +0000

    week13

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -35,6 +35,18 @@ elif 60 <= score < 70:
 else: 
     print("Grade: F")
 
+
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >= 80 and score < 90:
+    print("Grade: B")
+elif score >= 70 and score < 80:
+    print("Grade: C")
+if score >= 60 and score < 70:
+    print("Grade: D")
+elif score < 60:
+    print("Grade: F")  
+
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
