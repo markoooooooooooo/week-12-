253c5fbd80b0343cafc0ff64e9f6933c3afaf86a
markoooooooooooo <mimunoz4@cps.edu>
2025-11-14 21:02:31 +0000

    list

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -23,21 +23,68 @@ my_list = [1, 2, 3, 4, 5]
 print(my_list) # [1, 2, 3, 4, 5]
 print(type(my_list)) # <class 'list'>
 
-print(my_list[0])
-print(my_list[1:4])
-print(my_list[0:])
+# print(my_list[0])
+# print(my_list[1:4])
+# print(my_list[0:])
 
-my_list.append(6)
-my_list.append(7)
-my_list.append(8)
-print(my_list)
-my_list.extend([10,11,12,13,14])
-print(my_list)
+# my_list.append(6)
+# my_list.append(7)
+# my_list.append(8)
+# print(my_list)
+# my_list.extend([10,11,12,13,14])
+# print(my_list)
 
-my_list.extend(list(range(15, 515)))
-print(my_list)
-my_list.extend(list(range(515, 1115)))
-print(my_list)
+# my_list.extend(list(range(15, 515)))
+# print(my_list)
+# my_list.extend(list(range(515, 1115)))
+# print(my_list)
+
+
+# new_list = ['a', 'b', 'c']
+# print(new_list)
+# new_list.append('d')
+# print(new_list)
+# removed_item = new_list.pop()
+# print(removed_item)
+# print(new_list)
+# remove_second_item = new_list.pop(1)
+# print(remove_second_item)
+# print(new_list)
+
+
+
+numbers = [4, 2, 5, 1, 3]
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+numbers.insert(2, 10)
+print(numbers)
+third_list = [7, 8, 9]
+third_list[0] = 6
+print(third_list)
+third_list[-1] = 10
+print(third_list)
+
+import random
+random_list = random.sample(range(1, 1000), 100)
+
+print(random_list)
+print(sorted(random_list))
+sorted_list = sorted(random_list)
+print(sorted_list)
+
+
+# summary of list functions
+# .append(item) - adds an item to the end of the list
+# .pop(index) - removes and returns the item at the specified index
+# .sort() - sorts the list in ascending order
+
+# .reverse() - reverses the order of the list
+
+
 
 # Practice Problems:
 
